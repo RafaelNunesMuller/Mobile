6e2453453927a3cb869ed67bc3d6bf98d1df01de
RafaelNunesMuller <rafaelmuller007@icloud.com>
2025-03-26 16:50:17 -0300

    style

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 // Only import react-native-gesture-handler on native platforms
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { BottomTabBar, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Home from './screen/screen';
 import Login from './screen/login';
 
 export default function App() {
-  const Drawer = createDrawerNavigator();
+  const BottomTabBar = createBottomTabNavigator();
   return (
 
-    <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name='Login' component={Login}/>
-        <Drawer.Screen name='Home' component={Home}/>
-      </Drawer.Navigator>
+    <NavigationContainer >
+      <BottomTabBar.Navigator 
+      initialRouteName='Login'
+      screenOptions={{
+        tabBarActiveTintColor:'black',
+        
+        tabBarActiveBackgroundColor:'red',
+
+        tabBarInactiveBackgroundColor:'blue',
+
+        headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+
+        headerTintColor: '#fff',
+
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+      >
+        <BottomTabBar.Screen name='Login' component={Login} options={{
+          
+        }}/>
+        <BottomTabBar.Screen name='Home' component={Home}/>
+
+
+      </BottomTabBar.Navigator>
     </NavigationContainer>
 
   );
