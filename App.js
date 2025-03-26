aad5be1c6d9026307ce89efc70df646aea257ed0
RafaelNunesMuller <rafaelmuller007@icloud.com>
2025-03-26 17:09:12 -0300

    icons

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 // Only import react-native-gesture-handler on native platforms
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { BottomTabBar, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 import Home from './screen/screen';
 import Login from './screen/login';
@@ -32,9 +34,15 @@ export default function App() {
       }}
       >
         <BottomTabBar.Screen name='Login' component={Login} options={{
-          
+          tabBarIcon:() => (
+            <MaterialIcons name="login" size={40} color="white"/>
+          ),
         }}/>
-        <BottomTabBar.Screen name='Home' component={Home}/>
+        <BottomTabBar.Screen name='Home' component={Home} options={{
+          tabBarIcon:() => (
+            <MaterialIcons name="home" size={40} color="white"  />
+          )
+        }}/>
 
 
       </BottomTabBar.Navigator>
